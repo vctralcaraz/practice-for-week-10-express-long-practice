558c69d1af95f453f5f6d86dc2b6e8bc7f3e588b
Victor Alcaraz <vctralcaraz@gmail.com>
2024-03-10 23:55:41 -0700

    phase 3 complete

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 require('express-async-errors');
 
+const dogsRouter = require('./routes/dogs');
+
 const app = express();
 
 app.use(express.json());
@@ -14,6 +16,8 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
+
+app.use('/dogs', dogsRouter);
 
 // For testing purposes, GET /
 app.get('/', (req, res) => {
