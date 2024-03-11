5f515920149592ab449573411403792ed51ce589
Victor Alcaraz <vctralcaraz@gmail.com>
2024-03-10 23:21:35 -0700

    phase 2 complete

--- server/app.js
+++ server/app.js
@@ -6,6 +6,15 @@ const app = express();
 app.use(express.json());
 app.use('/static', express.static('assets'));
 
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    console.log(res.statusCode);
+  });
+
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // For testing purposes, GET /
 app.get('/', (req, res) => {
   res.json("Express server running. No content provided at root level. Please use another route.");
@@ -22,6 +31,16 @@ app.post('/test-json', (req, res, next) => {
 // For testing express-async-errors
 app.get('/test-error', async (req, res) => {
   throw new Error("Hello World!")
+});
+
+app.use((req, res, next) => {
+  const error = new Error(`The requested resource couldn't be found.`);
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  console.log(`${err.statusCode} ${err.message}`);
 });
 
 const port = 8000;
